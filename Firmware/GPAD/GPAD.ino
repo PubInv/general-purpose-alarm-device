250ced6e63f5e12ab3093d0a3ca4cbbdab3ad386
Forrest Erickson <forresterickson@comcast.net>
2022-08-17 09:40:58 -0400

    Setup gpio pins for LIGHTs and Tone and move wink into function.

--- Firmware/GPAD/GPAD.ino
+++ Firmware/GPAD/GPAD.ino
@@ -1,14 +1,17 @@
-/* Copyright (C) 2021 Robert Read, Forrest Erickson.
- *  
+/* GPAD_Factory_Test.ino
+  This program for factory test of hardware
+
+  Copyright (C) 2021 Robert Read, Forrest Erickson.
+
   This program includes free software: you can redistribute it and/or modify
   it under the terms of the GNU Affero General Public License as
   published by the Free Software Foundation, either version 3 of the
   License, or (at your option) any later version.
-  
+
   See the GNU Affero General Public License for more details.
   You should have received a copy of the GNU Affero General Public License
   along with this program.  If not, see <https://www.gnu.org/licenses/>.
-  
+
   This program is distributed in the hope that it will be useful,
   but WITHOUT ANY WARRANTY; without even the implied warranty of
   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
@@ -28,10 +31,50 @@ unsigned long nextLEDchange = 100; //time in ms.
 #include <Wire.h>
 //For LCD
 #include <LiquidCrystal_I2C.h>
+LiquidCrystal_I2C lcd(0x27, 20, 4); // set the LCD address to 0x27 for a 16 chars and 2 line display
+
+//Pin definitions
+#define SWITCH_MUTE 2
+#define TONE_PIN 8
+#define LIGHT0 3
+#define LIGHT1 5
+#define LIGHT2 6
+#define LIGHT3 9
+#define LIGHT4 10
 
 
 
 //Functions
+
+void updateWink(void) {
+  //Wink the built in LED
+  if (((millis() - lastLEDtime) > nextLEDchange) || (millis() < lastLEDtime)) {
+    if (digitalRead(LED_BUILTIN) == LOW) {
+      digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
+      nextLEDchange = HIGH_TIME_LED;
+    } else {
+      digitalWrite(LED_BUILTIN, LOW);   // turn the LED on (HIGH is the voltage level)
+      nextLEDchange = LOW_TIME_LED;
+    }
+    lastLEDtime = millis();
+  }//end of Wink
+}
+
+void splashLCD(void) {
+  //LiquidCrystal_I2C lcd(0x27, 20, 4); // set the LCD address to 0x27 for a 16 chars and 2 line display
+  lcd.init();                      // initialize the lcd
+  // Print a message to the LCD.
+  lcd.backlight();
+  lcd.setCursor(0, 0);
+  lcd.print("********************");
+  lcd.setCursor(3, 1);
+  lcd.print("GPAD Starting");
+  lcd.setCursor(0, 2);
+  lcd.print("by Public Invention");
+  lcd.setCursor(0, 3);
+  lcd.print("********************");
+
+}
 
 //Scan I2C
 
@@ -68,9 +111,31 @@ void setup() {
   delay(100);
   pinMode(LED_BUILTIN, OUTPUT);      // set the LED pin mode
   digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
-
-
+  Serial.println("Start I2C scan");
   scanI2C();
+  Serial.println("End I2C scan");
+  Serial.println("Start LCD splash");
+  splashLCD();
+  Serial.println("EndLCD splash");
+
+
+  Serial.println("Set up GPIO pins");
+  pinMode(SWITCH_MUTE, INPUT_PULLUP);
+  //pinMode(TONE_PIN, OUTPUT); //Not necessary
+  int LIGHT[] = {3,5,6,9,10};
+  for(int i =0; i<5; i++){
+  Serial.println(LIGHT[i]);
+      pinMode(LIGHT[i],OUTPUT);
+  }
+  
+
+//#define TONE_PIN 8
+//#define LIGHT0 3
+//#define LIGHT1 5
+//#define LIGHT2 6
+//#define LIGHT3 9
+//#define LIGHT4 10
+Serial.println("end set up GPIO pins");
 
 
   digitalWrite(LED_BUILTIN, LOW);   // turn the LED off at end of setup
@@ -79,18 +144,6 @@ void setup() {
 void loop() {
   // put your main code here, to run repeatedly:
 
-  //Wink the LED
-  if (((millis() - lastLEDtime) > nextLEDchange) || (millis() < lastLEDtime)) {
-    if (digitalRead(LED_BUILTIN) == LOW) {
-      digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
-      nextLEDchange = HIGH_TIME_LED;
-    } else {
-      digitalWrite(LED_BUILTIN, LOW);   // turn the LED on (HIGH is the voltage level)
-      nextLEDchange = LOW_TIME_LED;
-    }
-    lastLEDtime = millis();
-  }//end of Wink
+  updateWink(); //The builtin LED.
 
-
-
-}
+}//end loop()
